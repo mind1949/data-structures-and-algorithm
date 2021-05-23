8e8265e94a41e250880130f4fc70d5eb7b552d5a
mind1949 <lianjie1949@gmail.com>
2021-05-23 16:12:40 +0800

    refactor

--- algs/sort/radix/radix_test.go
+++ algs/sort/radix/radix_test.go
@@ -6,5 +6,22 @@ import (
 )
 
 func TestSort(t *testing.T) {
-	sort.TestSortFn(t, Sort)
+	for i, s := range [][]int{
+		{1},
+		{2, 0},
+		{3, 3, 4, 2, 1, 10, 5},
+		{4, 3, 2, 1, 0, 10, 8, 21, 6},
+		{1, 9, 4, 9, 10, 1},
+		{100, 99, 30, 21, 5, 3, 10000, 9898, 7, 3, 3, 4, 2, 1, 10, 5},
+	} {
+		before := make([]int, len(s))
+		copy(before, s)
+
+		Sort(s)
+		if !sort.IsSorted(s) {
+			t.Fatalf("expect cases[%d] is sorted, but got: %v ---> %v", i, before, s)
+		} else {
+			t.Logf("%v ---> %v", before, s)
+		}
+	}
 }
--- algs/sort/sort.go
+++ algs/sort/sort.go
@@ -8,7 +8,7 @@ func TestSortFn(t *testing.T, sortFn func(s []int)) {
 		copy(before, s)
 
 		sortFn(s)
-		if !isSorted(s) {
+		if !IsSorted(s) {
 			t.Fatalf("expect cases[%d] is sorted, but got: %v ---> %v", i, before, s)
 		} else {
 			t.Logf("%v ---> %v", before, s)
@@ -20,14 +20,14 @@ func getCases() [][]int {
 	return [][]int{
 		{1},
 		{2, 0},
-		{3, 3, 4, 2, 1, 10, 5},
+		{3, 3, -4, 2, 1, 10, 5},
 		{4, 3, 2, 1, 0, 10, 8, 21, 6},
 		{1, 9, 4, 9, 10, 1},
-		{100, 99, 30, 21, 5, 3, 10000, 9898, 7, 3, 3, 4, 2, 1, 10, 5},
+		{100, 99, 30, 21, 5, 3, -10000, 9898, 7, 3, 3, 4, 2, 1, 10, 5},
 	}
 }
 
-func isSorted(nums []int) bool {
+func IsSorted(nums []int) bool {
 	for i := 0; i < len(nums)-1; i++ {
 		if nums[i] > nums[i+1] {
 			return false
